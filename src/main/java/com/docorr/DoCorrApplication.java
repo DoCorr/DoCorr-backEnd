9541f6099e81e71de3ff723bf4fd18b472111c05
Bamdule <Bamdule5@gmail.com>
2020-02-22 13:28:13 +0900

    base spring boot

---
+++ src/main/java/com/docorr/DoCorrApplication.java
@@ -0,0 +1,13 @@
+package com.docorr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoCorrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoCorrApplication.class, args);
+	}
+
+}
